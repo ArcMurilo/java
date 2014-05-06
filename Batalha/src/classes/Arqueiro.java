38a259af6154bd3095c9067b17d5113861800d31
ArcMurilo <murilo.moraes91@gmail.com>
2014-05-06 14:43:13 -0300

    Update Arqueiro.java

--- Batalha/src/classes/Arqueiro.java
+++ Batalha/src/classes/Arqueiro.java
@@ -3,10 +3,10 @@ package classes;
 import java.util.Random;
 
 public class Arqueiro extends Personagem{
-    private final int modVida = 4;
-    private final float modForca = 1.8;
-    
+
     public Arqueiro() {
+        modVida = 4;
+        modForca = 1.8;
         this.calculaVida(this.modVida);
         this.calculaForca(this.modForca);
         this.agilidade = 1.5;

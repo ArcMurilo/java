76744dd4f1eb3dcccb631d7d420c6b7f74a76186
ArcMurilo <murilo.moraes91@gmail.com>
2014-05-06 14:41:57 -0300

    Update Guerreiro.java

--- Batalha/src/classes/Guerreiro.java
+++ Batalha/src/classes/Guerreiro.java
@@ -3,10 +3,10 @@ package classes;
 import java.util.Random;
 
 public class Guerreiro extends Personagem {
-    private final int modVida = 5;
-    private final float modForca = 2.5;
-    
+
     public Guerreiro() {
+        modVida = 5;
+        modForca = 2.5;
         this.calculaVida(this.modVida);
         this.calculaForca(this.modForca);
         this.agilidade = 1;

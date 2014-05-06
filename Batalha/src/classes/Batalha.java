4010e4d8ec78a267837c9d2dc7d1637b8ad80243
ArcMurilo <murilo.moraes91@gmail.com>
2014-05-06 14:17:51 -0300

    implentado batalha

--- Batalha/src/classes/Batalha.java
+++ Batalha/src/classes/Batalha.java
@@ -4,6 +4,7 @@
  */
 package classes;
 
+import java.util.Random;
 /**
  *
  * @author Murilo
@@ -11,4 +12,26 @@ package classes;
 public class Batalha {
     private Personagem player1;
     private Personagem player2;
+    private String vencedor;
+    private Random numRand;
+
+    public Batalha(Personagem player1, Personagem player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    
+    public boolean fimBatalha() {
+        if (player1.getVida() <= 0) {
+            this.vencedor = player2.getNome();
+            return true;
+        }
+        if (player2.getVida() <= 0) {
+            this.vencedor = player1.getNome();
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
 }
